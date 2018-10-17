f3b13d3422e2348fe215142195db7f2a0b8f10f5
antoneville <antoneville@hotmail.com>
2018-10-17 17:22:04 +0100

    Part 1 Complete

---
+++ BankAccount.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ExerciseOne
+{
+    public abstract class BankAccount
+    {
+        private String accountNumber;
+        private double balance;
+
+        public String AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            set { this.balance = value; }
+        }
+
+        public BankAccount(String accNumber)
+        {
+            this.accountNumber = accNumber;
+            this.balance = 0;
+        }
+
+        public abstract void MakeDeposit();
+        public abstract void MakeWithDrawals();
+        public static void Main()
+        {
+
+            //BankAccount h2 = new BankAccount();
+            //h2.Balance = 400.89;                      
+            //Console.WriteLine(h2.Balance);        //Testing Set Property for Balance
+
+            // BankAccount x2 = new BankAccount("AIB314666P");
+            // Console.WriteLine(x2.accountNumber);
+            // Console.WriteLine(x2.balance);       //Testing Constructor
+
+
+
+        }
+    }
+
+    public class CurrentAccount : BankAccount
+    {
+        private double limit;
+
+
+        public double Limit
+        {
+            get { return limit; }
+        }
+
+        public CurrentAccount(double limit, string accNumber)
+            : base(accNumber)
+        {
+            this.limit = limit;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" ");
+        }
+
+        public override void MakeWithDrawals()
+        {
+            if (limit < Balance)
+            {
+                Console.WriteLine("You have reached the limit of your Bank Account");
+            }
+            else
+            {
+                Console.WriteLine("New Balance: {0}", Balance);
+                Console.WriteLine("Transaction Completed");
+            }
+        }
+    }
+
+    public enum TransactionType
+    {
+        Deposit, Withdrawal
+    }
+
+    // an account transaction
+    public class AccountTransaction
+    {
+        private TransactionType type;       // deposit/withdrawal
+        private double amount;          // amount concerned
+
+        // constructor
+        public AccountTransaction(TransactionType type, double amount)
+        {
+            this.type = type;
+            this.amount = amount;
+        }
+
+        // return human readable String
+        public override String ToString()
+        {
+            return "type: " + type + " amount: " + amount;
+        }
+    }
+
+
+}
